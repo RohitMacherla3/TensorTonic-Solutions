3ebff5525d6aedd968debeaed8f2ced9ab8f6533
Rohit Macherla <rm1667@scarletmail.rutgers.edu>
2026-03-08 13:23:19 -0400

    Update solution for dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -5,7 +5,7 @@ def dot_product(x, y):
     Compute the dot product of two 1D arrays x and y.
     Must return a float.
     """
-    # Write code here
+
     x_np = np.asarray(x)
     y_np = np.asarray(y)
     return np.dot(x_np, y_np)
